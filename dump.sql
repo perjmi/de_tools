139bef6586517848208de58370ad2cfe5ef09e1e
cp <perjmi@yahoo.com>
2023-10-19 13:55:24 +0200

    v6

--- dump.sql
+++ dump.sql
@@ -3,7 +3,7 @@
 --
 
 -- Dumped from database version 12.15 (Debian 12.15-1.pgdg120+1)
--- Dumped by pg_dump version 12.16 (Ubuntu 12.16-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 14.9 (Ubuntu 14.9-0ubuntu0.22.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -21,51 +21,25 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: address; Type: TABLE; Schema: public; Owner: postgres
+-- Name: sales; Type: TABLE; Schema: target; Owner: postgres
 --
 
-CREATE TABLE public.address (
-    address_id integer DEFAULT nextval('public.address_address_id_seq'::regclass) NOT NULL,
-    address character varying(50) NOT NULL,
-    address2 character varying(50),
-    district character varying(20) NOT NULL,
-    city_id smallint NOT NULL,
-    postal_code character varying(10),
-    phone character varying(20) NOT NULL,
-    last_update timestamp without time zone DEFAULT now() NOT NULL
+CREATE TABLE target.sales (
+    logon character varying NOT NULL,
+    first_name character varying NOT NULL,
+    store_id smallint NOT NULL,
+    sales_id character varying NOT NULL
 );
 
 
-ALTER TABLE public.address OWNER TO postgres;
+ALTER TABLE target.sales OWNER TO postgres;
 
 --
--- Name: address address_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: sales sales_pkey; Type: CONSTRAINT; Schema: target; Owner: postgres
 --
 
-ALTER TABLE ONLY public.address
-    ADD CONSTRAINT address_pkey PRIMARY KEY (address_id);
-
-
---
--- Name: idx_fk_city_id; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE INDEX idx_fk_city_id ON public.address USING btree (city_id);
-
-
---
--- Name: address last_updated; Type: TRIGGER; Schema: public; Owner: postgres
---
-
-CREATE TRIGGER last_updated BEFORE UPDATE ON public.address FOR EACH ROW EXECUTE FUNCTION public.last_updated();
-
-
---
--- Name: address fk_address_city; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.address
-    ADD CONSTRAINT fk_address_city FOREIGN KEY (city_id) REFERENCES public.city(city_id);
+ALTER TABLE ONLY target.sales
+    ADD CONSTRAINT sales_pkey PRIMARY KEY (sales_id);
 
 
 --
